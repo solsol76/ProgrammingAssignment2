4861a51a8c80eb6f84276c2d8c8ae66c5e4dbe4b
solsol76 <svetla_shopova@abv.bg>
2014-04-26 23:29:21 -0400

    Solution

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## This script contains two functions using for caching inverse matrix
+
+
+## This function creates a matrix object that can keep its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+}
+
+
+## This functions check if an inverse matrix for x  exists and if it exists
+##  the function will display it, otherwise it will cache it and display it
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
